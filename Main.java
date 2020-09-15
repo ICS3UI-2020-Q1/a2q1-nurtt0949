13321b29335b6666c7c0e71eeb3bc4688ec6d2a2
nurtt0949 <nurtt0949@wrdsb.ca>
2020-09-15 14:39:11 +0000

    Odd or Even integer

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
  *
- * @author 
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +11,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //create a Scanner for user input
+   Scanner input = new Scanner(System.in);
+  
+   //ask user for integer
+   System.out.println("Enter an integer");
+
+   //get integer from user
+   int integer = input.nextInt();
+
+   //get the remaider
+   int remainder = integer % 2;
+
+   //determine if integer is even or odd
+   if(remainder > 2){
+
+   //tell them they picked an odd number 
+   System.out.println("The integer you chose was odd");
+
+   } else {
+
+   //tell them they picked an even number
+   System.out.println("The integer you chose was even ");
+
+   } 
   }
 }
